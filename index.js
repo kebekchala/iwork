0a23804d559ed3d63f33245e3c8a2e9af44834ef
kebekchala <chalakebek@gmail.com>
2022-01-27 23:20:08 +0300

    update

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const { Builder ,By, Key } = require('selenium-webdriver');
     await driver.get("https://www.iworkplc.com/contact");
     
     let mobile = await driver.findElement(By.xpath('//*[@id="comp-kktfmgn8"]/p/span')).getText();
-    //console.log(mobile);
+    console.log("Mobile Number",mobile);
     let office = await driver.findElement(By.xpath('//*[@id="comp-ks7htvty"]/p/span')).getText();
-    //console.log(office);
+    console.log("Office Number", office);
 
     let mobileNumber = mobile.slice(mobile.length - 4);
     let officeNumber = office.slice(mobile.length - 5);
-    console.log(mobileNumber," =>", officeNumber);
+    console.log(officeNumber," / ", mobileNumber);
     let result = officeNumber / mobileNumber;
 
     console.log("result => ", result);
